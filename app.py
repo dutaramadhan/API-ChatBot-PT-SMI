927efb085f91336de6a6ea7eb5c0a42e0fbfa2a6
dutaramadhan <duta2711@gmail.com>
2024-01-25 09:54:01 +0700

    feat and fix: add chat completion

--- app.py
+++ app.py
@@ -1,27 +1,15 @@
 from flask import Flask, request, jsonify, Response, send_file, abort
 from dotenv import load_dotenv
 import requests
-from chat_completion import chat_completion
+from chat_completion import run_conversation
 
 app = Flask(__name__)
-
-api_query_url = r'http://10.10.6.69:5000/smi/api/embedding/query'
-
-
-def fecth_query_api(query):
-    try:
-        response = requests.get(api_query_url, params={'query': query})
-        return response.json()
-    except Exception as e:
-        abort(400, str(e))
-
 
 @app.route('/smi/chatbot', methods=['GET'])
 def chatbot():
     try:
         query = request.args.get('query')
-        query_result = fecth_query_api(query)
-        result = chat_completion(query, query_result['result'])
+        result = run_conversation(query)
         return jsonify(result) 
     except Exception as e:
         error_message = {'error': str(e)}
--- chat_completion.py
+++ chat_completion.py
@@ -3,53 +3,107 @@ from dotenv import load_dotenv
 import os
 import json
 import requests
+from flask import abort
 
 load_dotenv()
 
 client = OpenAI(api_key = os.getenv('API_KEY'))
+api_query_url = r'http://10.10.6.69:5000/smi/api/embedding/query'
 
-url = "https://api.openai.com/v1/chat/completions"
+def search_data(query):
+    try:
+        response = requests.get(api_query_url, params={'query': query})
+        result = response.json().get('result')
+        data = ""
+        for i, res in enumerate(sorted(result, key=lambda x: x['similarity'], reverse=True)[0:6]):
+            data += "\n\ndata " + str(i+1) + '\n'
+            #data += "\nsource title : " + res.get('source_title')
+            uri = 'http://10.10.6.69:5000/files/' + res.get('source_uri')
+            data += "\nsource url : " + uri + '\n'
+            #data += "\ncontent : " + res.get('content')
+            data += res.get('source_title') + ' ' + res.get('content')
+        return data
+    except Exception as e:
+        abort(400, str(e))
 
-def chat_completion(query, query_result):
-    system_prompt = ""
-    for i, content in enumerate(query_result):
-        system_prompt += '\ndata ' + str(i+1) + '\n' + query_result[i]['source_title'] + '\n' + query_result[i]['content'] + '\n' + "URL Sumber: " + query_result[i]['source_uri'] + '\n'
-
+def run_conversation(query):
+    messages = [{"role": "system", "content": "You are professional assistant. answer question based on data. you can search data with function call. Dont search multiple data in 1 query instead split into multiple query. always include the source on answer and source url. dont answer a question that didnt relate to PT SMI and the data that have been given before"},
+                {"role": "user", "content": query}]
+    tools = [
+        {
+            "type": "function",
+            "function": {
+                "name": "search_data",
+                "description": "Get relevan data from knowledge base with vector search",
+                "parameters": {
+                    "type": "object",
+                    "properties": {
+                        "query": {
+                            "type": "string",
+                            "description": "query to search data"
+                        }
+                    }, 
+                    "required": ["query"]
+                }
+            }
+            
+    }
+    ]
     response = client.chat.completions.create(
         model="gpt-3.5-turbo-1106",
-        response_format={ "type": "json_object" },
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant designed to output JSON. Your name is chatS MI. You only answer question that related to PT SMI and i will include data that will be inserted as a additional knwoledge for you. Please include the source name and URL from on answer"},
-            {"role": "system", "content": system_prompt},
-            {"role": "user", "content": query}
-        ]
+        messages=messages,
+        tools=tools,
+        tool_choice="auto",
     )
-    print (response)
-    return response.choices[0].message.content
+    response_message = response.choices[0].message
+    tool_calls = response_message.tool_calls
 
-    # payload = json.dumps({
-    #     "model": "gpt-3.5-turbo",
-    #     "temperature": 0.25,
-    #     "messages": [
-    #         {
-    #             "role": "system",
-    #             "content": "You are a helpful assistant. Your name is chatS MI. You only answer question that related to PT SMI and i will include data that will be inserted as a additional knwoledge for you. Please include the source name and URL from on answer"
-    #         },    
-    #         {
-    #             "role": "system",
-    #             "content": system_prompt
-    #         },
-    #         {
-    #             "role": "user",
-    #             "content": query
-    #         }
-    #     ]
-    # })
-    # headers = {
-    #     'Authorization': 'Bearer ' + os.getenv('API_KEY'),
-    #     'Content-Type': 'application/json'
-    # }
+    if tool_calls:
+        available_functions = {
+            "search_data": search_data,
+        }
+        messages.append(response_message)
 
-    # response = requests.request("POST", url, headers=headers, data=payload)
+        for tool_call in tool_calls:
+            function_name = tool_call.function.name
+            function_to_call = available_functions[function_name]
+            function_args = json.loads(tool_call.function.arguments)
+            function_response = function_to_call(query = function_args.get("query"))
+            messages.append(
+                {
+                    "tool_call_id": tool_call.id,
+                    "role": "tool",
+                    "name": function_name,
+                    "content": function_response,
+                }
+            )
+        second_response = client.chat.completions.create(
+            model="gpt-3.5-turbo-1106",
+            messages=messages,
+        )
+        second_response = {
+            "finish_reason": second_response.choices[0].finish_reason,
+            "index": second_response.choices[0].index,
+            "logprobs": second_response.choices[0].logprobs,
+            "message": {
+                "content": second_response.choices[0].message.content,
+                "role": second_response.choices[0].message.role,
+                "function_call": second_response.choices[0].message.function_call,
+                "tool_calls": second_response.choices[0].message.tool_calls
+            }
+        }
 
-    # return response.json()['choices'][0]['message']['content']
+        return second_response
+    else:
+        response = {
+            "finish_reason": response.choices[0].finish_reason,
+            "index": response.choices[0].index,
+            "logprobs": response.choices[0].logprobs,
+            "message": {
+                "content": response.choices[0].message.content,
+                "role": response.choices[0].message.role,
+                "function_call": response.choices[0].message.function_call,
+                "tool_calls": response.choices[0].message.tool_calls
+            }
+        }
+        return response
