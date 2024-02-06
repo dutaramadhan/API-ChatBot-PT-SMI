2bb3b130e760ce03de95d5a1e13f83d4b4f8c765
dutaramadhan <duta2711@gmail.com>
2024-02-06 12:30:56 +0700

    fix: add temperature and top_p to second response

--- chat_completion.py
+++ chat_completion.py
@@ -81,6 +81,8 @@ def run_conversation(query):
         second_response = client.chat.completions.create(
             model="gpt-3.5-turbo-1106",
             messages=messages,
+            top_p=0.5,
+            temperature=0.5,
         )
         return second_response
     else:
